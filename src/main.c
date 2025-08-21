a898bb8c3cd67b7561bb257501f3504c27fc9b0f
Damian Parrino <bucanero@users.noreply.github.com>
2025-08-21 18:12:27 -0300

    Update Makefile

--- src/main.c
+++ src/main.c
@@ -20,18 +20,6 @@
 #include "sha1.h"
 #include "ps2mc.h"
 
-#define SEED_OFFSET 0x8
-#define HASH_OFFSET 0x1c
-#define TYPE_OFFSET 0x3C
-
-#define PSV_MAGIC 0x50535600
-
-const uint8_t key[2][0x10] = {
-							{0xFA, 0x72, 0xCE, 0xEF, 0x59, 0xB4, 0xD2, 0x98, 0x9F, 0x11, 0x19, 0x13, 0x28, 0x7F, 0x51, 0xC7},
-							{0xAB, 0x5A, 0xBC, 0x9F, 0xC1, 0xF4, 0x9D, 0xE6, 0xA0, 0x51, 0xDB, 0xAE, 0xFA, 0x51, 0x88, 0x59}
-						};
-const uint8_t iv[0x10] = {0xB3, 0x0F, 0xFE, 0xED, 0xB7, 0xDC, 0x5E, 0xB7, 0x13, 0x3D, 0xA6, 0x0D, 0x1B, 0x6B, 0x2C, 0xDC};
-
 
 int extractPSU(const char *save);
 
@@ -53,14 +41,8 @@ static void usage(char *argv[])
 {
 	printf("This tool converts and resigns PS1/PS2 saves to PS3 .PSV savegame format.\n\n");
 	printf("USAGE: %s <filename>\n\n", argv[0]);
-	printf("INPUT FORMATS\n");
-	printf(" .mcs            PS1 MCS File\n");
-	printf(" .psx            PS1 AR/GS/XP PSX File\n");
-	printf(" .cbs            PS2 CodeBreaker File\n");
-	printf(" .max            PS2 ActionReplay Max File\n");
-	printf(" .xps            PS2 Xploder/SharkPort File\n");
-	printf(" .psu            PS2 EMS File (uLaunchELF)\n");
-	printf(" .psv            PS3 PSV File (to PS1 .mcs/PS2 .psu)\n\n");
+	printf("INPUT FORMAT\n");
+	printf(" .psu            PS2 EMS File (uLaunchELF)\n\n");
 	return;
 }
 
